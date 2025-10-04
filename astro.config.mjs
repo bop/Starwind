10638d6156e5c141df3444aa07c019b41d5b9254
Baptiste <baptiste.ossipow@gmail.com>
2025-10-04 15:34:11 +0200

    Starwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bop.github.io/starwind/',
+    site: 'https://bop.github.io/Starwind/',
   integrations: [mdx(), sitemap()],
 
   vite: {
