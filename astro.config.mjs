aa891c0ad327a6a8ea7b621238fb3b294cbc92e7
Baptiste <bossip@ik.me>
2025-11-02 11:41:26 +0100

    typo ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://bop.github.io',
-    base: '/Starwind/',
+    base: '/Starwind',
   integrations: [mdx(), sitemap()],
 
   vite: {
